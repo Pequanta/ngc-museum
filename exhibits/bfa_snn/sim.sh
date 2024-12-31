82ec6307791c476225690d16adccb56e0a8dd800
Pequanta <penielyohanness6@gmail.com>
2024-12-31 10:24:26 +0300

    The library been modified to work with bfa_snn model - the modified parts are the directory path and input data

--- exhibits/bfa_snn/sim.sh
+++ exhibits/bfa_snn/sim.sh
@@ -2,10 +2,10 @@
 ################################################################################
 # Simulate the BFA-SNN on the MNIST database
 ################################################################################
-DATA_DIR="../../data/mnist"
+DATA_DIR="/content/ngc-museum/data/"
 
-rm -r exp/* ## clear out experimental directory
-python train_bfasnn.py  --dataX="$DATA_DIR/trainX.npy" \
+rm -r /content/ngc-museum/exhibits/bfa_snn/exp/* ## clear out experimental directory
+python3 /content/ngc-museum/exhibits/bfa_snn/train_bfasnn.py  --dataX="$DATA_DIR/trainX.npy" \
                         --dataY="$DATA_DIR/trainY.npy" \
                         --devX="$DATA_DIR/validX.npy" \
                         --devY="$DATA_DIR/validY.npy" \
